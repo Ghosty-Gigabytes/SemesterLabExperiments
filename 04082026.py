3457a07bf07d95a737914eb84dec05dc97e14768
Yatharth Shahi <boleto.imspooky@gmail.com>
2026-08-04 14:21:43 +0530

    Wrote a python program to implement logic gates

--- 04082026.py
+++ 04082026.py
@@ -1,2 +1,23 @@
 ## Write a python program to implement logic gates
 
+A = int(input("First input: "))
+B = int(input("Second input: "))
+
+AND = A and B;
+OR = A or B;
+XOR = A ^ B;
+NOT_A = not A;
+NOT_B = not B;
+NAND = not AND;
+NOR = not OR;
+XNOR = not XOR;
+
+print("Logic Gate Outputs");
+print("AND = ", int(AND));
+print("OR = ", int(OR));
+print("XOR = ", int(XOR));
+print("NOT_A = ", int(NOT_A));
+print("NOT_B = ", int(NOT_B));
+print("NAND = ", int(NAND));
+print("NOR = ", int(NOR));
+print("XNOR = ", int(XNOR));
